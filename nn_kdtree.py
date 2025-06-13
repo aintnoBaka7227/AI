7679286df9d0f88176fa14fcd53dc91d60c6ad25
Phong <a1873825@adelaide.edu.au>
2025-06-13 14:52:46 +0930

    handle dimension 12

--- nn_kdtree.py
+++ nn_kdtree.py
@@ -107,7 +107,7 @@ def main():
     train_P = train.values.astype(float)
     test_P = test.values.astype(float)
     
-    root = BuildKdTree(train_P, input_dimension % 11)
+    root = BuildKdTree(train_P, input_dimension % 12)
     
     left_count = 0
     right_count = 0
